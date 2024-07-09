bb49178579671be21bf67f5039da1ad541e31df5
松山佑真 <rinmoku4818@yahoo.co.jp>
2024-07-09 19:10:07 +0900

    static change
    静的サイト公開用に切り替え

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from flask_app.init import create_app
 app = create_app()
 
 #テスト用
-#if __name__ == "__main__":
-#    app.run(host='0.0.0.0',debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
--- flask_app/init.py
+++ flask_app/init.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     # appの設定
-    app = Flask(__name__, static_folder="static/",instance_relative_config=True)
+    app = Flask(__name__, static_folder="data/",instance_relative_config=True)
     app.config.from_pyfile('config.py')
 
     # Blueprintの登録
